1fe46f3ac449bdebceec9394ee5f32e837d0c428
pbojinov <petarbojinov@gmail.com>
2014-04-02 17:38:57 -0700

    remove all the flatui redesign, lets keep it simple KISS

--- make-nw.sh
+++ make-nw.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# zip -r desktop-notify.nw * -x app/ app/**\* .git/**\*
-# ignore some repos we aren't using the the demo so we can build faster
 
-zip -r desktop-notify.nw * -x app/bower_components/handlebars.js/**\* app/bower_components/open-sans-fontface/**\*
+# all resources for demo are local now
+zip -r desktop-notify.nw * -x app/ app/**\* .git/**\*
+
+# ignore some repos we aren't using the the demo so we can build faster
+# zip -r desktop-notify.nw * -x app/bower_components/handlebars.js/**\* app/bower_components/open-sans-fontface/**\*
